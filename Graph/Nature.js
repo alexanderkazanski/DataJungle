30e4f2ea0cec87e2f7151b17ffd3cbe26742296e
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-12-18 14:42:41 -0500

    add spider web

---
+++ Graph/Nature.js
@@ -0,0 +1,67 @@
+class SpiderWeb {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  // add a vertex
+  addAnchorThread(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      this.adjacencyList[vertex] = [];
+    }
+  }
+
+  // add a edge 
+  addFrameThread(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) this.addAnchorThread(vertex1);
+    if (!this.adjacencyList[vertex2]) this.addAnchorThread(vertex2);
+    this.adjacencyList[vertex1].push(vertex2);
+    this.adjacencyList[vertex2].push(vertex1);
+  }
+
+  // remove a edge
+  removeFrameThread(vertex1, vertex2) {
+    this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(vertex => vertex !== vertex2);
+    this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(vertex => vertex !== vertex1);
+  }
+
+  // remove a vertex
+  removeAnchorThread(vertex) {
+    while(!this.adjacencyList[vertex]?.length) {
+      let adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeFrameThread(vertex, adjacentVertex)
+    }
+    delete this.adjacencyList[vertex];
+  }
+
+  // display the graph
+  display() {
+    for (let vertex in this.adjacencyList) {
+      console.log(vertex, ":", this.adjacencyList[vertex])
+    }
+  }
+
+}
+
+const graph = new SpiderWeb()
+
+graph.addAnchorThread('Wall Art');
+graph.addAnchorThread("Walked by Mindset Coach")
+graph.addFrameThread("Wall Art", "Walked by Mindset Chach")
+
+graph.addAnchorThread('ChatGPT');
+graph.addFrameThread("ChatGPT", "Walked by Mindset Chach")
+
+graph.addAnchorThread('Decided to sprint during my walk')
+graph.addFrameThread("ChatGPT", "Decided to sprint during my walk");
+
+
+graph.addAnchorThread("Scrimba");
+graph.addAnchorThread("Speader at Meetup referenced JavaScript frequently and Job search")
+graph.addFrameThread("Scrimba", "Speader at Meetup referenced JavaScript frequently and Job search")
+
+graph.addAnchorThread("Wild Life")
+
+graph.addAnchorThread("Sora generated wild life")
+graph.addFrameThread("Wild Life", "Sora generated wild life")
+
+graph.display();
